d3d7e70bd0bba15ba2eff98b1f963a5b17acfb04
C13L99 <6krBy96tu0@gmail.com>
2026-05-22 23:26:21 -0600

    Create translateTime()
    This function loads the time as JST time, then converts it into local time.
    
    For now, local timezone is static

--- main.go
+++ main.go
@@ -1,4 +1,35 @@
 package main
+func translateTime(timeStr string, toTZ string) (string, error) {
+	// Kindly provided and debugged by and with Claude
+	// Don't worry, it won't explode, I already tested it ;)
+
+	// Load both Local and Tokyo timezones
+	fromLoc, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		return "", err
+	}
+
+	toLoc, err := time.LoadLocation(toTZ)
+	if err != nil {
+		return "", err
+	}
+
+	// FIX: Incorrect time by 01:04:00
+	// Why the fuck are they "historically accurate"? They think I'm gonna travel
+	// back in time???
+
+	now := time.Now().In(fromLoc)
+	datePrefix := now.Format("2006-01-02")
+
+	// Convert our string into LocalTimezone
+	t, err := time.ParseInLocation("2006-01-02 15:04", datePrefix+" "+timeStr, fromLoc)
+	if err != nil {
+		return "", err
+	}
+
+	// Finally convert our LocalTime obj to Tokyo
+	return t.In(toLoc).Format("15:04"), nil
+}
 func main() {
 	args := os.Args[1:]
 
