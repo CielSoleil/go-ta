afcdcec5c8a14cbab25ccb872e2d7e3042e59bd5
C13L99 <6krBy96tu0@gmail.com>
2026-05-22 23:20:42 -0600

    Import old regexes

--- main.go
+++ main.go
@@ -6,4 +6,11 @@ func main() {
 
 	log.Printf("Hour passed: %s\n", run_hour)
 
+	// Regexes
+	re_hour, _ := regexp.Compile(`^(\d{2}):(\d{2})$`)
+	re_special, _ := regexp.Compile(`^([noteaimdgh]+)$`)
+	re_nplus, _ := regexp.Compile(`^(now)\s\+\s(\d+)\s([minuteshordaywk]+)$`)
+	re_tomorrow, _ := regexp.Compile(`^(\d{2}):(\d{2})\stomorrow$`)
+	re_day, _ := regexp.Compile(`^(\d{2}):(\d{2})\s([mondaytueswhrfi]+)$`)
+
 }
